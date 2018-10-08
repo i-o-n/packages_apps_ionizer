a5f8c0afdce83d5f9bb4a14127c0285c0fd3a266
Shubham Singh <coolsks94@gmail.com>
2018-10-08 19:50:59 +0530

    Allow users to switch between Pie/Oreo recents [3/3]

--- src/com/ion/ionizer/fragments/Recents.java
+++ src/com/ion/ionizer/fragments/Recents.java
@@ -17,8 +17,11 @@ package com.ion.ionizer.fragments;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
@@ -26,20 +29,49 @@ import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
-import com.android.internal.logging.nano.MetricsProto; 
+import com.android.settings.R;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.ion.IonUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.ion.ionizer.R;
+import java.util.Arrays;
+import java.util.HashSet;
 
-public class Recents extends SettingsPreferenceFragment {
+public class Recents extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
-    public static final String TAG = "Recents";
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private ListPreference mRecentsComponentType;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.ion_settings_recents);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            IonUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        }
+        return false;
     }
 
     @Override
