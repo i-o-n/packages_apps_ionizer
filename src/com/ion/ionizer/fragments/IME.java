Havoc-OS/android_packages_apps_HavocSettings

Use new SeekBar

src/com/havoc/settings/fragments/IME.java
@@ -37,7 +37,7 @@ import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen; 
  
 import com.android.settings.SettingsPreferenceFragment; 
-import com.havoc.settings.preferences.CustomSeekBarPreference; 
+import com.havoc.settings.preferences.SystemSettingSeekBarPreference; 
 import com.android.settings.R; 
 import android.provider.Settings; 
 import android.view.Gravity; 

i-o-n/packages_apps_ionizer

src/com/ion/ionizer/fragments/IME.java
@@ -37,7 +37,7 @@ import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen; 
  
 import com.android.settings.SettingsPreferenceFragment; 
-import com.ion.ionizer.preferences.CustomSeekBarPreference; 
+import com.ion.ionizer.preferences.SystemSettingSeekBarPreference; 
 import com.android.settings.R; 
 import android.provider.Settings; 
 import android.view.Gravity; 