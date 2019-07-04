b4685cc895c586b16b4248e02bf2bbe971ce3dad
Lup Gabriel <gwolf2u.website@gmail.com>
2019-07-04 23:31:22 +0530

    GameMode: remove hwkeys toggle on non-hwkeys devices

--- src/com/ion/ionizer/fragments/GamingMode.java
+++ src/com/ion/ionizer/fragments/GamingMode.java
@@ -74,6 +74,11 @@ public class GamingMode extends SettingsPreferenceFragment
     private Map<String, Package> mGamingPackages;
     private Context mContext;
 
+    private static final int KEY_MASK_HOME = 0x01;
+    private static final int KEY_MASK_BACK = 0x02;
+    private static final int KEY_MASK_MENU = 0x04;
+    private static final int KEY_MASK_APP_SWITCH = 0x10;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,6 +90,10 @@ public class GamingMode extends SettingsPreferenceFragment
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
         mHardwareKeysDisable = (SwitchPreference) findPreference(GAMING_MODE_HW_KEYS);
+
+        if (!hasHWkeys()) {
+            prefScreen.removePreference(mHardwareKeysDisable);
+        }
 
         mPackageManager = getPackageManager();
         mPackageAdapter = new PackageListAdapter(getActivity());
@@ -208,6 +217,19 @@ public class GamingMode extends SettingsPreferenceFragment
         }
 
     };
+
+    private boolean hasHWkeys() {
+        final int deviceKeys = getContext().getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        return (hasHomeKey || hasBackKey || hasMenuKey || hasAppSwitchKey);
+    }
 
     private void refreshCustomApplicationPrefs() {
         if (!parsePackageList()) {
