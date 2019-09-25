f1a99b7d093d059f5739fc46efbf1a91c7d92aa1
AnkitGourav <Ankitgourav035@gmail.com>
2019-09-25 09:22:45 +0530

    move to androidx

--- src/com/ion/ionizer/fragments/Animations.java
+++ src/com/ion/ionizer/fragments/Animations.java
@@ -19,11 +19,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto; 
