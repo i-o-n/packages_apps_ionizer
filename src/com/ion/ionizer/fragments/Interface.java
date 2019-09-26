1ff7e15441dc469f99c3eaf002161dc3989fc5d1
AnkitGourav <Ankitgourav035@gmail.com>
2019-09-27 01:48:30 +0530

    move theming to ionizer

--- src/com/ion/ionizer/fragments/Interface.java
+++ src/com/ion/ionizer/fragments/Interface.java
@@ -15,36 +15,54 @@
  */
 package com.ion.ionizer.fragments;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.provider.Settings;
 
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import com.android.internal.logging.nano.MetricsProto; 
-import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 
 import com.ion.ionizer.R;
 
-public class Interface extends SettingsPreferenceFragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static final String TAG = "Interface";
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.ion_settings_interface);
-    }
+public class Interface extends DashboardFragment {
+    private static final String TAG = "Interface";
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.ION_IONIZER;
+        return MetricsEvent.ION_IONIZER;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.ion_settings_interface;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.accent_color"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        return controllers;
     }
 }
